b1776dc99930e93fb8b13d53977a42797b386af8
Ilya <ilya.tatarinov212@gmail.com>
2024-07-22 00:00:22 +0300

    added the ability to add movies and ratings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 "use strict";
 
-alert("hello");
+const numberOfFilms = +prompt("how many movies have you already watched so far?")
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let oneWatchedMovie = prompt("The name of one of the last movies you watched?");
+personalMovieDB.movies[oneWatchedMovie] = prompt("How much would you rate it?");
+
+console.log(personalMovieDB);
